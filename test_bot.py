a62099aded1e99022d24de3ef6ab6860d33cff56
reymswernek1793 <166836803+reymswernek1793@users.noreply.github.com>
2024-04-12 19:25:49 +0500

    Update test_bot.py
    version 2

--- test_bot.py
+++ test_bot.py
@@ -15,4 +15,26 @@ def start(message):
                types.InlineKeyboardButton('USD', callback_data='USD'),
                types.InlineKeyboardButton('RUB', callback_data='RUB'))
 
+ bot.send_message(message.chat.id,"Choose a currency: ", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_query(call):
+    querystring = {'from': call.data, 'to':'UZS','amount': '1'}
+
+    headers={
+        "X-RapidAPI-Key": "bd262d54c7mshf47276b2ceb8ad0p10d8f7jsn038b77b71d63",
+        "X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com"
+    }
+
+    response=requests.get(API, headers=headers, params=querystring)
+
+    if response.status_code==200:
+        data=response.json()
+        conversion_result=data['result']
+        bot.send_message(call.message.chat.id, f'Conversion result: {conversion_result}')
+    else:
+        bot.send_message(call.message.chat.id, 'Failed to perform conversion. Please try again later')
+
+print("The bot is running....")
+bot.polling()
 
